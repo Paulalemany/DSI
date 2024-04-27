3043c013235ea4eaf1721149acebb26de49b61ef
Paulalemany <palemany@ucm.es>
2024-04-27 13:31:37 +0200

    no consigo poner la img por código

--- DSI-Practica1/practica1-DSI/Assets/Scripts/Lab4c.cs
+++ DSI-Practica1/practica1-DSI/Assets/Scripts/Lab4c.cs
@@ -13,12 +13,42 @@ public class Lab4c : MonoBehaviour
 
         VisualTreeAsset template_cajas = Resources.Load<VisualTreeAsset>("Templates/ContenedorCajas");
 
-        VisualElement cajasTop1     = template_cajas.Instantiate();
-        VisualElement cajasTop2     = template_cajas.Instantiate();
-        VisualElement cajasBottom   = template_cajas.Instantiate();
+        #region Parte 1
+        //VisualElement cajasTop1     = template_cajas.Instantiate();
+        //VisualElement cajasTop2     = template_cajas.Instantiate();
+        //VisualElement cajasBottom   = template_cajas.Instantiate();
+
+        //top.Add(cajasTop1);
+        //top.Add(cajasTop2);
+        //bottom.Add(cajasBottom);
+        #endregion
+
+        #region Parte 2
+        //for (int i = 0; i < 6; i++)
+        //{
+        //    top.Add(template_cajas.Instantiate());
+        //    bottom.Add(template_cajas.Instantiate());
+        //}
+        #endregion
+
+        VisualElement cajasTop1 = template_cajas.Instantiate();
+        VisualElement cajasTop2 = template_cajas.Instantiate();
+        VisualElement cajasBottom = template_cajas.Instantiate();
 
         top.Add(cajasTop1);
         top.Add(cajasTop2);
         bottom.Add(cajasBottom);
+
+        Sprite img_bear = Resources.Load<Sprite>("Images/oso");
+        Sprite img_wolf = Resources.Load<Sprite>("Images/Lobo");
+        Sprite img_cat = Resources.Load<Sprite>("Images/gato");
+
+        VisualElement caja1 = cajasTop1.Q("Caja1");
+        VisualElement caja2 = cajasTop2.Q("Caja2");
+        VisualElement caja3 = cajasBottom.Q("Caja3");
+        caja1.style.backgroundImage = new StyleBackground(img_bear);
+        caja2.style.backgroundImage = new StyleBackground(img_wolf);
+        caja3.style.backgroundImage = new StyleBackground(img_cat);
+
     }
 }
